ccfc29ebc4430cd7bd9e4bdd57f215f37e5c7d4d
Vladimir <viperezf@upbc.edu.mx>
2014-10-17 12:06:53 -0700

    Actividad 3: Revisada.

--- Ejecutable/Readme.txt
+++ Ejecutable/Readme.txt
@@ -2,3 +2,4 @@
 
 Actividad 1: 7, por la tardanza, pero Bien.
 Actividad 2: 10 bien.
+Actividad 3: 6.5 Falto Crear el metodo "iniciando()", falto mandarlo a llamar y hacer los cambios que te pedi.
--- Source/Game.cpp
+++ Source/Game.cpp
@@ -7,7 +7,24 @@
 CGame::CGame(){
 	estado = ESTADO_INICIANDO;//ACT2: Mal, aqui debes de poner tu estado inicial, por eso te marcara error.
 	atexit(SDL_Quit);
+	//ACT3:Mal, este codigo no va aqui.
 	///ACT3: Mal, este codigo de abajo, debera ir en un metodo inicializando.
+	//if (SDL_Init(SDL_INIT_VIDEO)){
+	//	printf("Error %s", SDL_GetError());
+	//	exit(EXIT_FAILURE);
+	//}
+	//screen = SDL_SetVideoMode ( 640, 480, 24, SDL_SWSURFACE );
+
+	//if (screen == NULL){
+	//	printf("Error %s ", SDL_GetError());
+	//	exit(EXIT_FAILURE);
+	//}
+	//SDL_Flip(screen); //este codigo estara provicionalmente aqui.
+	//SDL_WM_SetCaption("Mi primer Juego", NULL);
+}
+
+//ACT3: Mal, Falto crear el metodo "iniciando()"
+void CGame::Iniciando(){
 	if (SDL_Init(SDL_INIT_VIDEO)){
 		printf("Error %s", SDL_GetError());
 		exit(EXIT_FAILURE);
@@ -18,7 +35,7 @@ CGame::CGame(){
 		printf("Error %s ", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
-	SDL_Flip(screen); //este codigo estara provicionalmente aqui.
+	
 	SDL_WM_SetCaption("Mi primer Juego", NULL);
 }
 
@@ -37,6 +54,8 @@ bool CGame::Start()
 		//Maquina de estados
 		switch(estado){
 			case Estado::ESTADO_INICIANDO: //INICIALIZAR
+				Iniciando();//ACT3: Mal, falto mandar a llamr este metodo.
+				estado = ESTADO_MENU;
 			break;
 			case Estado::ESTADO_MENU:	//MENU
 			break;
